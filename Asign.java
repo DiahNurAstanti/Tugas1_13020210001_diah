1882716fd10eb65f296e6457ac6cdfcb23541faa
DIAH NUR ASTANTI <127530115+DiahNurAstanti@users.noreply.github.com>
2023-03-11 16:30:21 +0800

    Create Asign.java

---
+++ Asign.java
@@ -0,0 +1,17 @@
+public class Asign {
+
+    /**
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* kamus */
+        int i;
+        /* Program */
+        System.out.print("hello\n"); i=5;
+        System.out.println("Ini nilai i : " +i);
+    }
+
+}
